d1e82d4e4ca79ff5180f2808242e59807bccb880
abowman-max <abowman@stay-on-target.net>
2026-06-08 22:01:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ div[data-baseweb="select"] input {
 
 /* keep multiselect controls from crushing selected labels */
 .stMultiSelect div[data-baseweb="select"] > div {
-    min-height: 30px !important;
+    min-height: 36px !important;
     align-items: center !important;
 }
 
